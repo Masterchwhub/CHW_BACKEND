86616030f102ad515e8d41b1324764b1c64d4ebc
kelthaz <davidkelthaz@gmail.com>
2023-12-27 17:51:09 -0500

    install cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import express from "express";
+import cors from 'cors'
 import loginRouter from './routes/login/login.routes'
 
-const app = express();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/', loginRouter);
