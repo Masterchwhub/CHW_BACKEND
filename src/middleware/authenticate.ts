ddb19b3dd9d177a3a02ab1332e69b969e6bb47cc
Prueba <prueba@MacBook-Pro-de-Prueba.local>
2023-12-28 17:29:10 -0500

    Descripción detallada de los cambios

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,15 +1,34 @@
-import { Request, Response, NextFunction } from 'express';
 
-export const authenticateMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+
+interface AuthenticatedRequest extends Request {
+  user?: any; // Puedes ajustar el tipo según la estructura de tu usuario
+}
+
+export const authenticateMiddleware = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ message: 'Missing authorization token' });
   }
 
-  // Aquí deberías agregar la lógica para verificar y validar el token
-  // Puedes utilizar bibliotecas como 'jsonwebtoken' para trabajar con tokens JWT
+  try {
+    // Verificar y decodificar el token
+    const decoded = jwt.verify(token, 'tu_secreto_secreto'); // Reemplaza 'tu_secreto_secreto' con tu secreto real
 
-  // Si el token es válido, puedes continuar con la siguiente función de middleware
-  next();
+    // Puedes hacer más validaciones aquí según tus necesidades
+    // Por ejemplo, verificar la expiración, el emisor, etc.
+
+    // Adjunta la información del usuario decodificada al objeto de solicitud para que esté disponible en las rutas protegidas
+    req.user = decoded;
+
+    // Continuar con la siguiente función de middleware
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: 'Invalid authorization token' });
+  }
 };
